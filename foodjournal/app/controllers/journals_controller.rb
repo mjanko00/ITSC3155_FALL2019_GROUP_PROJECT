c9e43d217b992212b1ecf809a6ee34b6832a9796
Fayliette Lewis <flewis2@uncc.edu>
2019-10-25 12:56:38 +0000

    Complete Create and Show

--- foodjournal/app/controllers/journals_controller.rb
+++ foodjournal/app/controllers/journals_controller.rb
@@ -1,9 +1,21 @@
 class JournalsController < ApplicationController
+    def show
+        @journal = Journal.find(params[:id])
+    end
     
     def new
     end
     
     def create
-        render plain: params[:journal].inspect
+        @journal = Journal.new(journal_params)
+        
+        @journal.save
+        redirect_to @journal
+        #render plain: params[:journal].inspect
     end
 end
+
+private
+    def journal_params
+    params.require(:journal).permit(:title, :text)
+    end
