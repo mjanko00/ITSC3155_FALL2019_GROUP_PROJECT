d58de405a521b2e38bd9076e79f71f333fdc6174
nodom2 <nodom2@uncc.edu>
2019-11-12 00:21:21 +0000

    add motivates routes

--- foodjournal/config/routes.rb
+++ foodjournal/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  resources :journals
+  resources :journals do
+    resources :motivates
+  end
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
