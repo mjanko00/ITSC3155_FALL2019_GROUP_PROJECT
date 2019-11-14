460a7a454fb66f057a6fdbcde02faf91aecdd942
”kimclaug” <kimclaug@uncc.edu>
2019-11-14 18:25:03 +0000

    made the journal.rb file match our previous validations

--- foodjournal/app/models/journal.rb
+++ foodjournal/app/models/journal.rb
@@ -1,2 +1,12 @@
 class Journal < ApplicationRecord
+    
+    has_many :motivates
+    
+    validates :meal_type, presence: true
+    validates :food_item, presence: true, length: { minimum: 5 }
+    validates :serving, presence: true, length: { minimum: 1 }
+    validates :cal, presence: true, length: { minimum: 1 }
+    validates :prot, presence: true, length: { minimum: 1 }
+    validates :carbs, presence: true, length: { minimum: 1 }
+    validates :fats, presence: true, length: { minimum: 1 }
 end
