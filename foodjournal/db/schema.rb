7d9d487c00efc3ea0a42ae6ffb8a52b93f864122
”kimclaug” <kimclaug@uncc.edu>
2019-11-15 00:24:31 +0000

    changed column types with new migration

--- foodjournal/db/schema.rb
+++ foodjournal/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_171638) do
+ActiveRecord::Schema.define(version: 2019_11_15_001758) do
 
   create_table "journals", force: :cascade do |t|
     t.string "food_item"
