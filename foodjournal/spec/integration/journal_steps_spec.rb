e418923b0bd19e18f970458363e5ea114962c2c6
Michael Janko <mjanko@uncc.edu>
2019-11-25 22:52:01 +0000

    Created a new rspec scenario (#2) and have it passing its tests

--- foodjournal/spec/integration/journal_steps_spec.rb
+++ foodjournal/spec/integration/journal_steps_spec.rb
@@ -1,13 +1,44 @@
 require 'rails_helper.rb'
 
 feature "Food journal user can sign up, log in, create a new food journal entry, and comment on their post." do
-    scenario "Food journal user successfuly navigates to the sign up page" do
+    scenario "Food journal user successfully navigates to the sign up page" do
        visit root_path
        expect(page).to have_content("My Food Journal")
-       click_link ("Sign Up")
+       click_link "Sign Up"
        expect(page).to have_content("New User")
        expect(page).to have_content("Name")
+       expect(page).to have_content("Email")
        expect(page).to have_content("Password")
        expect(page).to have_content("Password confirmation")
+    end
+    
+    scenario "Food journal user successfully creates a user" do
+        visit signup_path
+        expect(page).to have_content("New User")
+        fill_in "Name", with: "Test Name"
+        fill_in "Email", with: "TestEmail123@testing.com"
+        fill_in "Password", with: "Testing123"
+        fill_in "Password confirmation", with: "Testing123"
+        click_button "Create User"
+        expect(page).to have_content("User was successfully created.")
+        expect(page).to have_content("Name: Test Name")
+        expect(page).to have_content("Email: testemail123@testing.com")
+    end
+    
+    scenario "Food journal user successfully navigates to the log in page" do
+       visit root_path
+       expect(page).to have_content("My Food Journal")
+       click_link ("Log In")
+       expect(page).to have_content("Login")
+       expect(page).to have_content("Email")
+       expect(page).to have_content("Password")
+    end
+    
+    scenario "Food journal user successfully logs in to an existing user" do
+       visit login_path
+       expect(page).to have_content("Login")
+       fill_in "Email", with: "TestEmail123@testing.com"
+       fill_in "Password", with: "Testing123"
+       click_button ""
     end
 end
