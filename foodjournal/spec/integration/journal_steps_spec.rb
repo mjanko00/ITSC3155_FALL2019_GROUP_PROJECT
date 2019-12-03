ba8ba2ed6a68de78b9b2ecbd30ee4a4bed1be84a
Michael Janko <mjanko@uncc.edu>
2019-12-03 18:27:08 +0000

    Created scenarios for logging in, navigating to the new journl entry page, and creating a new food journal. However, these scenarios will not pass because of user authentication. I wanted to include them to meet the requirements and to implement in them in the future.

--- foodjournal/spec/integration/journal_steps_spec.rb
+++ foodjournal/spec/integration/journal_steps_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
 
-feature "Food journal user can sign up, log in, create a new food journal entry, and comment on their post." do
+feature "Food journal user can sign up, log in and create a new food journal entry" do
     scenario "Food journal user successfully navigates to the sign up page" do
        visit root_path
        expect(page).to have_content("My Food Journal")
@@ -34,6 +34,9 @@ feature "Food journal user can sign up, log in, create a new food journal entry,
        expect(page).to have_content("Password")
     end
     
+    #  All scenarios below this line will not be able to pass because of User Authentication
+    #  I am still writing them for future implementation
+    #-------------------------------------------------------------------------------------
     scenario "Food journal user succeffully logs in to an existing user" do
         visit login_path
         expect(page).to have_content("Login")
@@ -42,5 +45,36 @@ feature "Food journal user can sign up, log in, create a new food journal entry,
         click_button "Login"
         expect(page).to have_content("My Food Journal")
         expect(page).to have_content("Welcome Test Name!")
+    end
+    
+    scenario "Food journal user successfully naviagtes to the new journal entry page" do
+        visit sessions_path
+        expect(page).to have_content("My Food Journal")
+        expect(page).to have_content("Welcome Test Name!")
+        click_link ("View My Journal")
+        expect(page).to have_content("Food Entries")
+    end
+    
+    scenario "Food journal user successfully creates a new journal entry" do
+        visit new_journal_path
+        expect(page).to have_content("Food Entries")
+        click_button "New Journal Entry"
+        expect(page).to have_content("New Food Journal Entry")
+        select "Breakfast", from: "Meal Type"
+        fill_in "Food Item", with: "Food Item"
+        fill_in "Serving Size", with: "1"
+        fill_in "Calories", with: "100"
+        fill_in "Proteins", with: "50"
+        fill_in "Carbs", with: "35"
+        fill_in "Fats", with: "20"
+        click_button "Create Journal"
+        expect(page).to have_content("Food Entry:")
+        expect(page).to have_content("Meal Type: Breakfast") 
+        expect(page).to have_content("Food Name: Eggs")
+        expect(page).to have_content("Serving Size: 1") 
+        expect(page).to have_content("Calories: 100")
+        expect(page).to have_content("Proteins: 50")
+        expect(page).to have_content("Carbs: 35")
+        expect(page).to have_content("Fats: 20")
     end
 end
